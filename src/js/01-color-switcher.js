93dc924108d9ca3f6d882743fd160dc58f7f34d5
Sudnikevych <sudnikevych@gmail.com>
2022-10-25 19:22:05 +0300

    add 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', clickStart);
+btnStop.addEventListener('click', clickStop);
+
+function clickStart(evt) {
+  evt.target.disabled = true;
+  btnStop.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function clickStop(evt) {
+  btnStart.removeAttribute('disabled');
+  evt.target.disabled = true;
+  clearInterval(timerId);
+}
